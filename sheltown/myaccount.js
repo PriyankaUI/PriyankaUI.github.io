e23749c089191d61b107522bfe4412ddb04ca809
PriyankaUI <66872012+PriyankaUI@users.noreply.github.com>
2020-11-20 22:20:14 -0400

    update

---
+++ sheltown/myaccount.js
@@ -0,0 +1,18 @@
+$(document).ready( function() {
+		
+	   /* $('#mybookings a').on('click', function(e) {
+	       e.preventDefault();	
+			$("#verification").hide();
+			$("#bookings").show();
+
+		});
+
+	    $('#accountsetting a[href="#verification"]').on('click', function() {
+	       	
+			$("#bookings").hide();
+			$("#verification").show();
+
+		});*/
+
+		
+	});
