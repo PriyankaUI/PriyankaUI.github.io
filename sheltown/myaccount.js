2b13fa5d981e5bf1b41c67f7c161d4893065e14b
PriyankaUI <66872012+PriyankaUI@users.noreply.github.com>
2020-11-20 23:18:40 -0400

    update

--- sheltown/myaccount.js
+++ sheltown/myaccount.js
@@ -1,18 +1,16 @@
-$(document).ready( function() {
-		
-	   /* $('#mybookings a').on('click', function(e) {
-	       e.preventDefault();	
-			$("#verification").hide();
-			$("#bookings").show();
+/*function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 
-		});
-
-	    $('#accountsetting a[href="#verification"]').on('click', function() {
-	       	
-			$("#bookings").hide();
-			$("#verification").show();
-
-		});*/
-
-		
-	});
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();*/
